feabab958571fd956b6928bf62ec6598363a4444
Petabyte Studios <brikbusters@gmail.com>
2019-07-17 13:14:37 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ var x = 0;
 var y = 0;
 
 function getMouse(event) {
-	if (false) {
+	if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 		x = 60 - (event.clientX + (window.innerWidth))/30;
 		y = 30 - (event.clientY + (window.innerHeight))/30;
 
