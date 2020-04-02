46e000f60ace6ba143b23cf9b62a33aea8e801a1
Daniel <brikbusters@gmail.com>
2020-04-02 19:02:32 -0400

    CLEAN EVERYTHING

--- javascript.js
+++ javascript.js
@@ -7,6 +7,5 @@ function getMouse(event) {
 		y = 30 - (event.clientY + (window.innerHeight))/30;
 
 		document.getElementById('main').style.backgroundPosition = x + "px " + y + "px" ;
-		//document.getElementsByTagName('body')[0].style.textShadow = "" + ((event.clientX + (window.innerWidth))/30) + "px " + (60 + (event.clientY + (window.innerHeight))/30) + "px 1px black";
 	}
 }
